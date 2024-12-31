46a6d9ceea58d1faf9d78e39094bce7fbdfdc008
Vishal <vishalkumarsinghv60@gmail.com>
2024-12-31 22:30:52 +0530

    Second Commit

---
+++ Exception Handling/src/customexception/Iss_Pyaar_Ko_Kya_Naam_Du.java
@@ -0,0 +1,9 @@
+package customexception;
+
+public class Iss_Pyaar_Ko_Kya_Naam_Du 
+{
+	public static void main(String[] args) {
+		System.out.println("Iss Pyaar Ko kya naam duuu..............");
+	}
+
+}
